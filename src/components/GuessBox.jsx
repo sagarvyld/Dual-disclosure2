c21cfe684841decc6831b705c6f7934e0d65842f
Sagar <sagar.kapoor@vyld.io>
2024-08-02 15:16:30 +0530

    Done

---
+++ src/components/GuessBox.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import { useState,useEffect,useRef } from "react";
+import Profile from "../assets/Profile.png";
+import Profile2 from "../assets/Profile2.png";
+const GuessBox = ({setIsEmpty, isEmpty ,setword ,topic ,emojies, answer, forward , send , right , word}) => {
+  const textareaRef = useRef(null);
+  const [message, setmessage] = useState("kjdska");
+  const [class_set, setclass]=useState("");
+  const emojis = emojies.split(' ');
+  const colorStyle = !right ? { color: 'var(--Info-Error-Error-Light, #FF4567)' } : { color: '' };
+  const [kill, setKill] = useState(false);
+  function countWords(sentence) {
+    return sentence.trim().split(/\s+/).filter(Boolean).length;
+  }
+
+  useEffect(() => {
+    setword('');
+    const timer = setTimeout(() => {
+      setKill(true);
+
+    }, 100);
+
+    return () => clearTimeout(timer); 
+  }, []);
+  const handleFocus = () => {
+    if (textareaRef.current) {
+      textareaRef.current.setAttribute('inputmode', 'text');
+      textareaRef.current.setAttribute('enterkeyhint', 'done');
+    }
+  };
+
+  const handleBlur = () => {
+    if (textareaRef.current) {
+      textareaRef.current.removeAttribute('inputmode');
+      textareaRef.current.removeAttribute('enterkeyhint');
+    }
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      if (textareaRef.current) {
+        textareaRef.current.blur();
+      }
+    }
+  };
+    const handleTextareaChange = (event) => {
+        const value = event.target.value.trim(); 
+       setword(value);
+        if (value === '') {
+          setIsEmpty(true);
+        } else {
+          setIsEmpty(false);
+        }
+      };
+    
+  return (
+    <div className={`guess_main_container_dual `}>
+      <div className={`guess_svg_dual `}>
+      <div className={`_real_svg_dual _notsend_width_dual `}>
+      <svg width="83" height="84" viewBox="0 0 83 84" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M-21.9606 77.4657L-21.9609 77.4655C-29.0356 72.3462 -30.6243 62.436 -25.5155 55.3706L-25.3499 55.1416L-25.3445 55.1307L-12.1163 36.9523L-9.27861 33.0527L-13.8658 31.5639L-35.4331 24.5639C-35.4768 24.5476 -35.5278 24.5294 -35.5841 24.5111C-43.88 21.7793 -48.4186 12.8419 -45.7198 4.56438L-45.7198 4.56431C-43.0282 -3.69155 -34.1199 -8.26846 -25.8073 -5.66068C-25.7222 -5.63029 -25.6342 -5.60238 -25.5436 -5.57747C-25.5362 -5.57544 -25.5286 -5.57335 -25.5207 -5.57122L-4.00485 1.38045L0.567101 2.85763L0.571213 -1.94704L0.590626 -24.6413C0.590626 -24.6414 0.590626 -24.6414 0.590626 -24.6415C0.598667 -33.3773 7.7324 -40.5 16.5047 -40.5C25.2781 -40.5 32.4109 -33.3761 32.4193 -24.6508L32.429 -1.93887L32.431 2.86798L37.0051 1.39011L58.6613 -5.60699L58.6614 -5.60703C67.0171 -8.30711 76.0112 -3.72547 78.7196 4.57432L78.7197 4.57444C81.4266 12.8687 76.8535 21.8419 68.5129 24.5476L69.5929 27.8768L68.5128 24.5476L46.8561 31.5734L42.268 33.0618L45.1061 36.9619L58.45 55.2993L58.4515 55.3017L58.5137 55.3878C63.6167 62.4561 62.0334 72.3574 54.9608 77.4752L54.9592 77.4764C47.8926 82.5961 37.9587 81.0541 32.7852 74.0593C32.7616 74.0265 32.7321 73.9864 32.6977 73.942L19.3223 55.6116L16.495 51.7368L13.6676 55.6116L0.356163 73.8544L0.345715 73.8648L0.189606 74.0756C-2.90312 78.2505 -7.71855 80.4886 -12.6263 80.4886C-15.8768 80.4886 -19.1428 79.5052 -21.9606 77.4657ZM51.438 33.7665L50.7661 32.8432L47.9361 34.9026L49.0161 38.2318L50.1023 37.8794L61.3029 53.2714C61.3098 53.2783 61.3155 53.285 61.3208 53.2921C61.3265 53.2998 61.3318 53.3081 61.3377 53.3175L61.3514 53.3391C67.5836 61.9714 65.652 74.0592 57.0127 80.3107C48.3831 86.5628 36.2589 84.6758 29.9491 76.1106C29.9428 76.1015 29.9371 76.0935 29.9308 76.0856L51.438 33.7665ZM58.3946 55.2119L58.3971 55.2158C58.3862 55.199 58.3788 55.1871 58.375 55.1809L58.3769 55.184L58.3809 55.1903L58.3946 55.2119Z" stroke="#0E1928" stroke-opacity="0.1" stroke-width="7"/>
+</svg>
+
+
+
+      </div>
+      </div>
+      <div className="guess_text_dual">Most random way that you have entertained yourself in the past week?</div>
+       <div className="Lie_Information_dual_2">
+          <div className="User_picture_dual_2">
+            <img src={Profile} alt="User" />
+          </div>
+          <div className="User_text_dual_2 backgorund_blur_d">{message}</div>
+        </div>
+      <div className="guess_symbol_dual"> 
+      <div className="emoji-container">
+      {emojis.map((emoji, index) => (
+        <span key={index} className="emoji">
+          {emoji}
+        </span>
+      ))}
+    </div>
+
+</div>
+ <div className="guess_answer_dual">
+  <textarea
+        ref={textareaRef}
+        onChange={handleTextareaChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        onKeyPress={handleKeyPress}
+        inputMode="text"
+        enterKeyHint="done"
+        placeholder="Type answer here"
+      ></textarea>
+      <img src={Profile2} className="Profile2_dual"/>
+      </div>
+      <div className="guessbox_text_d2">
+        <p>Answers are revealed when you both answer</p>
+      </div>
+    </div>
+  );
+};
+
+export default GuessBox;
---
+++ src/pages/Landingpage.jsx
@@ -0,0 +1,243 @@
+import React from "react";
+import GuessBox from "../components/GuessBox";
+import { useState, useEffect } from "react";
+import Confetti from "react-confetti";
+import profile from "../assets/Profile.png";
+import stringSimilarity from 'string-similarity';
+
+const Landingpage = ({ skip, setskip }) => {
+  const [word, setword] = useState("");
+  const [right, setright] = useState(false);
+  const [showConfetti, setShowConfetti] = useState(false);
+  const [message, setmessage] = useState("kjdska");
+  const [topic, settopic] = useState("");
+  const [answer, setanswer] = useState("");
+  const [emojies, setemoji] = useState("");
+  const [data_it, setdata_it] = useState(null);
+  const backward = () => {
+    setsend(false);
+    setIsEmpty(true);
+  };
+  const triggerConfetti = () => {
+    setShowConfetti(true);
+    setTimeout(() => setShowConfetti(false), 5000);
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const url = "https://vyld-cb-dev-api.vyld.io/api/v1/activity-games/game";
+    const params = new URLSearchParams({
+      activityId: urlParams.get("activityId"),
+    });
+    fetch(`${url}?${params}`, {
+      method: "GET",
+      headers: {},
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const Data_coming = data.data;
+        console.log(Data_coming);
+        setmessage(Data_coming.message);
+        settopic(Data_coming.reqD[0].topicArea);
+        setanswer(Data_coming.reqD[1].topic);
+        setemoji(Data_coming.reqD[2].Emoji);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+  const [isEmpty, setIsEmpty] = useState(true);
+  const [style, setstyle] = useState({});
+  const [send, setsend] = useState(false);
+
+  useEffect(() => {
+    if (right && send) {
+      triggerConfetti();
+    }
+  }, [right, send]);
+ 
+  const stopWords = ['the', 'a', 'an', 'in', 'at', 'to'];
+  const abbreviations = {
+    'dr': 'doctor',
+    'mr': 'mister',
+    'mrs': 'misses',
+    'st': 'street',
+    'jr': 'junior',
+    'sr': 'senior'
+  };
+  const normalizeString = str => {
+    return str
+      .toLowerCase()
+      .replace(/[^a-zA-Z0-9\s]/gi, '') 
+      .split(' ')
+      .filter(word => !stopWords.includes(word) && word) 
+      .map(word => abbreviations[word] || word)
+  };
+
+  const compareMovieTitles = (userInput, correctAnswer) => {
+    const normalizedUserInput = normalizeString(userInput);
+    const normalizedCorrectAnswer = normalizeString(correctAnswer);
+  
+    if (normalizedUserInput.join(' ') === normalizedCorrectAnswer.join(' ')) {
+      return true;
+    }
+
+    return normalizedUserInput.every((userWord, index) => {
+      if (index >= normalizedCorrectAnswer.length) return false; 
+      const correctWord = normalizedCorrectAnswer[index];
+      const similarity = stringSimilarity.compareTwoStrings(userWord, correctWord);
+      console.log(`Comparing "${userWord}" with "${correctWord}": Similarity = ${similarity}`);
+      return similarity > 0.7;
+    });
+  };
+  const forward = () => {
+    // console.log("forward");
+    if (!isEmpty) {
+      if (compareMovieTitles(word.toLowerCase(),answer.toLowerCase())) {
+        setright(true);
+      } else {
+        setright(false);
+      }
+      setsend(true);
+    }
+    const data = {
+      reqD: [{ topicArea: topic }, { topic: answer }, { Emoji: emojies }],
+      message: message,
+      isCorrect: right,
+    };
+    const staging_url = "https://vyld-cb-dev-api.vyld.io";
+    const url = `${staging_url}/api/v1/activity-games/game-response`;
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        // console.log(result);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  useEffect(() => {
+    if (isEmpty) {
+      setstyle({ opacity: 0.5 });
+    } else {
+      setstyle({});
+    }
+  }, [isEmpty, send]);
+  return (
+    <div className="app_container_dual">
+      {showConfetti && <Confetti />}
+      {
+        <div className="background_svg_dual">
+         <svg width="360" height="180" viewBox="0 0 360 180" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M388 -167C359.667 -165.333 238.407 19.8627 281 -7.5C363.5 -60.5 381.5 10.5 332.5 38.5C268 75.3571 157.755 170 232 170C281 170 263 67.5 184.5 89.5C106 111.5 62.613 155.588 39.5 102.5C7.5 29 118.5 -18 65 -93C15.3856 -162.553 -12 -102 -12 -69.5" stroke="#1D2939" stroke-width="19"/>
+</svg>
+
+        </div>
+      }
+      {!send ? (
+        <div className="upper_buttons_dual">
+          <button className="back_button">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M13.165 11.9934L13.1634 11.6393C13.1513 10.2348 13.0666 8.98174 12.9206 8.18763C12.9206 8.17331 12.7613 7.38572 12.6599 7.12355C12.5006 6.74463 12.2126 6.42299 11.8515 6.2192C11.5624 6.0738 11.2592 6 10.9417 6C10.6922 6.01157 10.2806 6.13714 9.98692 6.24242L9.74283 6.33596C8.12612 6.97815 5.03561 9.07656 3.85199 10.3598L3.76473 10.4495L3.37527 10.8698C3.12982 11.1915 3 11.5847 3 12.0077C3 12.3866 3.11563 12.7656 3.3469 13.0718C3.41614 13.171 3.52766 13.2983 3.62693 13.4058L4.006 13.8026C5.31046 15.1243 8.13485 16.9782 9.59883 17.5924C9.59883 17.6057 10.5086 17.9857 10.9417 18H10.9995C11.6639 18 12.2846 17.6211 12.6021 17.0086C12.6888 16.8412 12.772 16.5132 12.8352 16.2252L12.949 15.6813C13.0788 14.8067 13.165 13.465 13.165 11.9934ZM19.4967 13.5183C20.3269 13.5183 21 12.8387 21 12.0004C21 11.1622 20.3269 10.4825 19.4967 10.4825L15.7975 10.8097C15.1463 10.8097 14.6183 11.3417 14.6183 12.0004C14.6183 12.6581 15.1463 13.1912 15.7975 13.1912L19.4967 13.5183Z"
+                fill="white"
+              />
+            </svg>
+          </button>
+          <button className="skip_button" onClick={() => setskip(true)}>
+            Skip
+          </button>
+        </div>
+      ) : (
+        <div className="upper_cross_button" onClick={() => backward()}>
+          <button>
+            <svg
+              width="26"
+              height="26"
+              viewBox="0 0 26 26"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle
+                cx="13"
+                cy="13"
+                r="12.25"
+                stroke="white"
+                stroke-width="1.5"
+              />
+              <path
+                d="M9 9L17 17"
+                stroke="white"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M17 9L9 17"
+                stroke="white"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </button>
+        </div>
+      )}
+      <div className="heading_above_dual">
+        <p className="you_got_a_dual">You Got a</p>
+        <p className="Dual_Disclosure">Emoji charades</p>
+      </div>
+      <GuessBox
+        emojies={emojies}
+        isEmpty={isEmpty}
+        topic={topic}
+        answer={answer}
+        send={send}
+        forward={forward}
+        setword={setword}
+        setIsEmpty={setIsEmpty}
+        right={right}
+        word={word}
+      />
+      {!send && message !== "" && (
+        <div className="Lie_Information_dual">
+          <div className="User_picture_dual">
+            <img src={profile} alt="User" />
+          </div>
+          <div className="User_text_dual">{message}</div>
+        </div>
+      )}
+      
+        <button
+          className="SpotPage_Submit"
+          onClick={() => {
+            forward();
+          }}
+        >
+          <p style={style}>Reveal</p>
+        </button>
+    </div>
+  );
+};
+
+export default Landingpage;
